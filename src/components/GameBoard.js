adda532a9024040c6737f41e2525b5e3b15c6845
Tung Pham <tung.42@gmail.com>
2025-01-23 12:23:41 +0700

    Update GameBoard.js

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import { Button, Row, Col } from "react-bootstrap";
 import Dice3D from "./Dice3D";
-import RulesModal from "./RulesModal";
 
 const texts = ["Bầu", "Cua", "Tôm", "Cá", "Gà", "Nai"];
 
 const GameBoard = () => {
   const [dice, setDice] = useState([0, 0, 0]);
   const [result, setResult] = useState("");
-  const [showRulesModal, setShowRulesModal] = useState(false);
   const [isResultVisible, setIsResultVisible] = useState(false); // State to trigger result animation
 
   const rollDice = () => {
@@ -29,30 +27,9 @@ const GameBoard = () => {
     }, 2000); // 2-second delay
   };
 
-  const openRulesModal = () => {
-    setShowRulesModal(true);
-  };
-
-  const closeRulesModal = () => {
-    setShowRulesModal(false);
-  };
-
   return (
     <div className="game-board text-center position-relative">
       <h1>Bầu Cua</h1>
-      <Button
-        variant="primary"
-        onClick={openRulesModal}
-        className="position-absolute rulesBtn"
-        style={{
-          top: "25px",
-          right: "25px",
-          fontSize: "1rem",
-          padding: "0.5rem 1rem !important",
-        }}
-      >
-        Xem Luật Chơi
-      </Button>
       <Row>
         {dice.map((value, idx) => (
           <Col key={idx}>
@@ -67,7 +44,6 @@ const GameBoard = () => {
         Kết quả: {result}
       </p>{" "}
       {/* Apply fade-in animation */}
-      <RulesModal show={showRulesModal} handleClose={closeRulesModal} />
     </div>
   );
 };
