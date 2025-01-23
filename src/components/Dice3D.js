338040db81a5f5b8c9c503aa4128536e4d21ace0
Tung Pham <tung.42@gmail.com>
2025-01-23 08:02:29 +0700

    3D fix

--- src/components/Dice3D.js
+++ src/components/Dice3D.js
@@ -2,19 +2,22 @@ import React from "react";
 import "./Dice3D.css"; // Include CSS for dice styling
 
 const Dice3D = ({ value }) => {
+  // Define rotations for each dice face corresponding to each animal
   const rotations = [
-    "rotateX(0deg) rotateY(0deg)", // Side 1
-    "rotateX(-90deg) rotateY(0deg)", // Side 2
-    "rotateX(0deg) rotateY(90deg)", // Side 3
-    "rotateX(90deg) rotateY(0deg)", // Side 4
-    "rotateX(0deg) rotateY(-90deg)", // Side 5
-    "rotateX(180deg) rotateY(0deg)", // Side 6
+    "rotateX(0deg) rotateY(0deg)", // Bầu
+    "rotateX(0deg) rotateY(90deg)", // Cua
+    "rotateX(0deg) rotateY(180deg)", // Tôm
+    "rotateX(0deg) rotateY(-90deg)", // Cá
+    "rotateX(90deg) rotateY(0deg)", // Gà
+    "rotateX(-90deg) rotateY(0deg)", // Nai
   ];
+
+  const animals = ["bau", "cua", "tom", "ca", "ga", "nai"];
 
   return (
     <div className="dice-container">
       <div className="dice" style={{ transform: rotations[value] }}>
-        {["bau", "cua", "tom", "ca", "ga", "nai"].map((animal, index) => (
+        {animals.map((animal, index) => (
           <div key={index} className={`face face-${index + 1}`}>
             <img
               src={`/images/${animal}.png`}
