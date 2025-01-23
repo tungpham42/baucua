c5af1be5cf76d8701854fd5653fe18dac608ed21
Tung Pham <tung.42@gmail.com>
2025-01-24 06:36:00 +0700

    lucky

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import GameBoard from "./components/GameBoard";
+import LuckyGameBoard from "./components/LuckyGameBoard";
 import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <Container className="text-center col-12 p-0">
-      <GameBoard />
+      <LuckyGameBoard />
     </Container>
   );
 }
