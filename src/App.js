d373fa931e2ff094c02a7e2b0a59b80735ecc56c
Tung Pham <tung.42@gmail.com>
2025-01-24 06:14:46 +0700

    CSS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <Container className="text-center col-12 p-0">
+    <Container className="text-center col-12 my-lg-5 my-md-5 my-sm-0 p-0">
       <GameBoard />
     </Container>
   );
