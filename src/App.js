ecda5c3134fcf48fc2b492210e70105510298aa7
Tung Pham <tung.42@gmail.com>
2025-01-25 09:29:37 +0700

    lucky

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GameBoard from "./components/GameBoard";
 import LuckyGameBoard from "./components/LuckyGameBoard";
 import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <Container className="text-center col-12 p-0">
-      <LuckyGameBoard />
-    </Container>
+    <Router>
+      <Container className="text-center col-12 p-0">
+        <Routes>
+          <Route path="/" element={<GameBoard />} />
+          <Route path="/lucky" element={<LuckyGameBoard />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
